3dbc464fee193ea2af44dedf7cabfee0e7d16230
bapun <mrutyunjayagadanayak@yandex.com>
2025-03-23 12:24:04 +0530

    Cache function

--- internal/pokecache/cache.go
+++ internal/pokecache/cache.go
@@ -1,8 +1,21 @@
 package pokecache
 
-import "time"
+import (
+	"sync"
+	"time"
+)
 
 type cacheEntry struct {
 	createdAt time.Time
 	val       []byte
+}
+
+type Cache struct {
+	mu    sync.Mutex
+	items map[string]cacheEntry
+}
+
+func NewCache(interval time.Duration) error {
+
+	return nil
 }
