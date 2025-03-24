b46f695fe615ffc9aa8db652ee6c0e9b5ac24106
bapun <mrutyunjayagadanayak@yandex.com>
2025-03-24 10:45:52 +0530

    Added Cache logic

--- internal/pokecache/cache.go
+++ internal/pokecache/cache.go
@@ -15,7 +15,49 @@ type Cache struct {
 	items map[string]cacheEntry
 }
 
-func NewCache(interval time.Duration) error {
+func (c *Cache) Add(key string, data []byte) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	newCacheEntry := cacheEntry{
+		val:       data,
+		createdAt: time.Now(),
+	}
+	c.items[key] = newCacheEntry
+}
 
-	return nil
+func (c *Cache) Get(key string) ([]byte, bool) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	value, exists := c.items[key]
+
+	if !exists {
+		return nil, false
+	}
+	return value.val, true
+}
+
+func (c *Cache) reapLoop(interval time.Duration) {
+	ticker := time.NewTicker(interval)
+
+	go func() {
+		for {
+			<-ticker.C
+
+			c.mu.Lock()
+			now := time.Now()
+			for key, value := range c.items {
+				if now.Sub(value.createdAt) > interval {
+					delete(c.items, key)
+				}
+			}
+			c.mu.Unlock()
+		}
+	}()
+}
+func NewCache(interval time.Duration) *Cache {
+	newCache := &Cache{
+		items: make(map[string]cacheEntry),
+	}
+	newCache.reapLoop(interval)
+	return newCache
 }
